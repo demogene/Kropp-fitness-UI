d4329615489267dcac4357d575b25463ed34d620
Umed <umedeminov@list.ru>
2024-11-09 18:03:42 +0500

    last update

---
+++ js/loader.js
@@ -0,0 +1,9 @@
+(function() {
+    const mask = document.querySelector('.mask')
+    window.addEventListener('load', () => {
+        mask.classList.add('hide')
+        setTimeout(() => {
+            mask.remove()
+        }, 1000)
+    })
+})();
